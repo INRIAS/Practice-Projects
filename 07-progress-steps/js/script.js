3e1518b906232fe5edcfcc1a9c3b38e817b524fa
INRIAS <inrialva@gmail.com>
2024-09-11 23:22:26 -0500

    Progress Step Css Ok

---
+++ 07-progress-steps/js/script.js
@@ -0,0 +1,5 @@
+const progress=document.querySelector(".progress");
+const next=document.querySelector(".next");
+const prev=document.querySelector(".prev");
+const circle=document.querySelector(".circle");
+
